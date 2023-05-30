e68999f5019356e2af09d871fcd5275b58083c1e
Eliseu Bento <eliseu1264@gmail.com>
2023-05-30 20:37:09 -0300

    ren view - views

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from .view import home_page, about_page, contact_page, login_page, register_page
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
 	path('', home_page),
