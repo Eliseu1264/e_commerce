28a5ff896e423bf588ebbd4d35c9e489f452aef8
eliseu bento <Eliseu>
2023-05-31 16:05:29 -0300

    static_Files

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -112,13 +112,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIR = [
+        os.path.join(BASE_DIR, "static_local")
+    ]
