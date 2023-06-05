3a18d39738f1e085d1b298c6b1d5f53510c75c3a
eliseu bento <Eliseu>
2023-06-05 15:37:24 -0300

    final aula 09

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -12,10 +12,13 @@ urlpatterns = [
 	path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view), 
+    
     path('products/<int:pk>', ProductDetailView.as_view()),
     path('products-fbv/<int:pk>', product_detail_view), 
+    
 	path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
