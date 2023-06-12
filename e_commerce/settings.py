3dcf7c0e1b0d46cdc765bab310ad73c9320c8ac6
eliseu bento <Eliseu>
2023-06-12 17:35:39 -0300

    commit lookups

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -111,7 +111,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIR = [
         os.path.join(BASE_DIR, "static_local")
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Product
 
@@ -34,7 +35,12 @@ class ProductDetailView(DetailView):
 #Function Based View
 def product_detail_view(request, pk=None, *args, **kwargs):
     # instance = Product.objects.get(pk = pk)
-    instance = get_object_or_404(Product, pk = pk)
+    # instance = get_object_or_404(Product, pk = pk)
+    try:
+        instance = Product.objects.get(id = pk)
+    except Product.DoesNotExist:
+        print("Nenhum produto encontrado aqui!")
+        raise Http404("Esse produto não existe!")
     context = {
         'object': instance
     }
