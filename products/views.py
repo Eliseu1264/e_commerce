775542fbc73fd139e0ee473659f063f6f099692a
eliseu bento <Eliseu>
2023-06-13 15:52:22 -0300

    13-06

--- products/views.py
+++ products/views.py
@@ -36,11 +36,19 @@ class ProductDetailView(DetailView):
 def product_detail_view(request, pk=None, *args, **kwargs):
     # instance = Product.objects.get(pk = pk)
     # instance = get_object_or_404(Product, pk = pk)
-    try:
-        instance = Product.objects.get(id = pk)
-    except Product.DoesNotExist:
-        print("Nenhum produto encontrado aqui!")
-        raise Http404("Esse produto não existe!")
+    # try:
+    #     instance = Product.objects.get(id = pk)
+    # except Product.DoesNotExist:
+    #     # print("Nenhum produto encontrado aqui!")
+    #     raise Http404("Esse produto não existe!")
+    
+    qs = Product.objects.filter(id = pk)
+    # if qs.exists():
+    if qs.count() == 1:
+        instance = qs.first()
+    else:
+        raise Http404("Esse produto não existe!")    
+    
     context = {
         'object': instance
     }
