c7054a59ee75755d0ef0fed031ae64af8b519744
eliseu bento <Eliseu>
2023-06-26 16:41:28 -0300

    slug

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import home_page, about_page, contact_page, login_page, register_page
 from django.conf import settings
 from django.conf.urls.static import static
-from products.views import ProductListView, product_list_view, product_detail_view, ProductDetailView, ProductFeaturedListView, ProductFeaturedDetailView
+from products.views import ProductListView, product_list_view, product_detail_view, ProductDetailView, ProductFeaturedListView, ProductFeaturedDetailView, ProductDetailSlugView
 
 urlpatterns = [
 	path('', home_page),
@@ -16,10 +16,9 @@ urlpatterns = [
     path('products-fbv/', product_list_view), 
     path('products/<int:pk>', ProductDetailView.as_view()),
     path('products-fbv/<int:pk>', product_detail_view), 
-
     path('featured/', ProductFeaturedListView.as_view()),
     path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),    
-
+    path('products/<slug:slug>/', ProductDetailSlugView.as_view()),
 	path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
