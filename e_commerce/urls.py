d94231344fb0c4b179363ac42e862afe3e1f6958
unknown <eliseu1264u@gmail.com>
2023-08-04 17:44:50 -0300

    commit - aula-44

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,21 +1,24 @@
 from django.conf import settings
 from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from carts.views import cart_home
+
 from .views import (home_page, 
                     about_page, 
                     contact_page, 
-                    login_page,
+                    login_page, 
                     logout_page,
                     register_page
 )
+
 urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
-    path('cart/', cart_home, name='cart'),    
+    path('cart/', include("carts.urls", namespace="cart")),
     path('login/', login_page, name='login'),
     path('logout/', logout_page, name='logout'),
     path('register/', register_page, name='register'),
@@ -24,6 +27,7 @@ urlpatterns = [
     path('products/', include("products.urls", namespace="products")),
     path('admin/', admin.site.urls),
 ]
+
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
